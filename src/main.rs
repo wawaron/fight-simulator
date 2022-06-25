65c9d5da0a85c8de68a3aa0afe1d7441682c7018
wawaron <wawaron@runbox.com>
2022-06-25 16:34:13 -0400

    Setup a basic fight loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod fighter;
 
+use rand::{thread_rng, Rng};
+
 //------------------------------------------------------------------------------
 fn main() {
     let mut f1 = fighter::Fighter {
@@ -14,4 +16,39 @@ fn main() {
         offense: 50.0,
         defense: 50.0,
     };
+
+    let mut rng = thread_rng();
+    let mut fight_is_on = true;
+    while fight_is_on {
+        // Fighters choose an action:
+        // 1. Standing (1/3)
+        // 2. Attack (1/3)
+        // 3. Defense (1/3)
+        let f1_action: u8 = rng.gen_range(1..=3);
+        let f2_action: u8 = rng.gen_range(1..=3);
+
+        if f1_action == 2 {
+            if f2_action != 3 {
+                f2.health -= 10.0;
+            }
+        }
+
+        if f2_action == 2 {
+            if f1_action != 3 {
+                f1.health -= 10.0;
+            }
+        }
+
+        if (f1.health <= 0.0) || (f2.health <= 0.0) {
+            fight_is_on = false;
+
+            if (f1.health <= 0.0) && (f2.health <= 0.0) {
+                println!("Double KO!");
+            } else if f1.health <= 0.0 {
+                println!("{} wins!", f2.name);
+            } else {
+                println!("{} wins!", f1.name);
+            }
+        }
+    }
 }

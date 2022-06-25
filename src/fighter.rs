b41f5fb9065f7bb1a7c4fb139276bc4c4601607c
wawaron <wawaron@runbox.com>
2022-06-24 22:32:39 -0400

    Add module `fighter`

---
+++ src/fighter.rs
@@ -0,0 +1,7 @@
+#[derive(Debug)]
+pub struct Fighter {
+    pub name: String,
+    pub health: f64,
+    pub offense: f64,
+    pub defense: f64,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod fighter;
+
+//------------------------------------------------------------------------------
 fn main() {
-    println!("Fight!");
+    let mut f1 = fighter::Fighter {
+        name: "Ryu".to_string(),
+        health: 100.0,
+        offense: 50.0,
+        defense: 50.0,
+    };
+    let mut f2 = fighter::Fighter {
+        name: "Ken".to_string(),
+        health: 100.0,
+        offense: 50.0,
+        defense: 50.0,
+    };
 }
